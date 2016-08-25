62566f1c7c65856c3dfe2cd7a560e2fa3d96b634
Sunil Kumar C <sunilkumar.c682@gmail.com>
2016-08-25 21:24:08 +0530

    Single post of a parcel - done

--- controllers/parcels.js
+++ controllers/parcels.js
@@ -3,7 +3,15 @@ var random = require('../utilities/random_num_generator.js');
 
 module.exports.set = function(app) {
     app.get("/parcels/:id", function(req, res) {
-        res.status(200).send("Parcel Details");
+        var parcel_id = req.params.id;
+        db_connection.query("Select single * from parcels where parcel_id = " + parcel_id);
+        db_connection.on("row", function(result) {
+            if (result)
+                res.status(200).send(result);
+            else {
+                res.status(200).send("Could not find the record");
+            }
+        });
     });
 
     app.post("/parcels", function(req, res) {
@@ -21,14 +29,17 @@ module.exports.set = function(app) {
         var sender_phone_no = body.sender_phone_no;
         var receiver_phone_no = body.receiver_phone_no;
 
-        db_connection.query("INSERT INTO parcels(parcel_id, from_person_name, to_person_name, color)" +
-            " values($1, $2, $3, $4)", [parcel_id, from_person_name, to_person_name, color],
+        db_connection.query("INSERT INTO parcels(parcel_id, from_person_name, to_person_name, color, " +
+            "weight, service_price, sender_address, receiver_address, sender_phone_no, receiver_phone_no)" +
+            " values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", [parcel_id, from_person_name, to_person_name,
+            color, weight, service_price, sender_address, receiver_address, sender_phone_no, receiver_phone_no],
                 function(err, result) {
                     if (err) {
                         res.status(500).send("Error in Storing data");
+                    } else {
+                        res.status(200).send(result);
                     }
                 });
 
-        res.status(200).send("Success from POST");
     });
 }
