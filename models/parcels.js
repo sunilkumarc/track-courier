ec750b2f1f829da1cebe99e77f11748b76acc417
Sunil Kumar C <sunilkumar.c682@gmail.com>
2016-08-27 23:35:26 +0530

    Initialized with Sequelize
    Simple /test end point is working with Sequelize

---
+++ models/parcels.js
@@ -0,0 +1,53 @@
+"use strict"
+
+module.exports = function(sequelize, DataTypes) {
+    var Parcels = sequelize.define("Parcels", {
+        parcel_id: {
+            type: DataTypes.UUID,
+            primaryKey: true
+        },
+        from_person_name: {
+            type: DataTypes.STRING
+        },
+        to_person_name: {
+            type: DataTypes.STRING
+        },
+        color: {
+            type: DataTypes.STRING
+        },
+        weight: {
+            type: DataTypes.FLOAT
+        },
+        service_price: {
+            type: DataTypes.FLOAT
+        },
+        dispatched_on: {
+            type: DataTypes.STRING
+        },
+        expected_delivery: {
+            type: DataTypes.STRING
+        },
+        sender_address: {
+            type: DataTypes.STRING
+        },
+        receiver_address: {
+            type: DataTypes.STRING
+        },
+        current_location_lat: {
+            type: DataTypes.DOUBLE
+        },
+        current_location_long: {
+            type: DataTypes.DOUBLE
+        },
+        sender_phone_no: {
+            type: DataTypes.STRING
+        },
+        receiver_phone_no: {
+            type: DataTypes.STRING
+        }
+    }, {
+       tableName: 'parcels'
+    });
+
+    return Parcels;
+}
