93c5d9f6872799919244f58d783a4345098577bc
Sunil Kumar C <sunilkumar.c682@gmail.com>
2016-08-30 22:39:03 +0530

    Started with session handling
    Session handling started using:
    1. express-session
    2. cookie-parser
    3. passport

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var account_controller = require('./controllers/accounts.js');
 var parcels_controller = require('./controllers/parcels.js');
-// var db_connection = require('./models/database.js');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var app = express();
 
 // Middleware to parse json body
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret_key',
+    saveUninitialized: true,
+    resave: true
+}));
 
 app.get('/', function(req, res) {
     res.status(200).send("Server Working!");
