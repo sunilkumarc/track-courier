63a8bb8abba78facf619c7204cc11997b1071d22
Sunil Kumar C <sunilkumar.c682@gmail.com>
2016-09-01 23:58:37 +0530

    Streak keeping

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -1,5 +1,5 @@
 module.exports.set = function(app) {
     app.get("/account/details", function(req, res) {
-        res.status(200).send("Account Details Page");
+        res.status(200).send("Keepint the streek!");
     });
 }
