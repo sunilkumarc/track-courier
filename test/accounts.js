00d1dc5bba818e0e1d25e9b6f56590ddcf4d6bcc
Sunil Kumar C <sunilkumar.c682@gmail.com>
2016-09-26 20:26:29 +0530

    Started with sinon.js for db method stubs

--- test/accounts.js
+++ test/accounts.js
@@ -2,29 +2,47 @@ var expect  = require("chai").expect;
 var request = require("request");
 var sinon = require('sinon');
 var Accounts = require('../models').Accounts;
+var low = require('lowdb');
+var db = low('db.json');
+
+db.defaults({ Accounts: [] }).value();
 
 describe("Accounts controller", function() {
-    // beforeEach(function() {
-    //     sinon.stub(Accounts, 'create', function(req, res) {
-    //         console.log('Inside stubbed method! :D');
-    //         // done(100);
-    //         console.log(req);
-    //     })
-    // });
-    //
-    // afterEach(function() {
-    //     Accounts.create().restore();
-    // });
-    //
-    // describe("Test Stubbing using sinon.js", function() {
-    //     it("Test Accounts.create", function(done) {
-    //         Accounts.create(function(res) {
-    //             expect(res).to.equal(100);
-    //             done();
-    //         });
-    //     });
-    // });
+    describe("Test Stubbing using sinon.js", function() {
+        it("Test Accounts.create", function(done) {
+            var create = sinon.stub(Accounts, 'create');
+            create.yields(100);
 
+            Accounts.create(function(res) {
+                expect(res).to.equal(100);
+                done();
+            });
+        });
+    });
+
+    /*
+    beforeEach(function() {
+        sinon.stub(Accounts, 'create', function(req, res) {
+            db.get('Accounts').push({username: 'test_user', name: 'Test User', email_id: 'test@gmail.com', password: 'test'}).value();
+            done(100);
+        });
+    });
+
+    afterEach(function() {
+        Accounts.create().restore();
+    });
+
+    describe("Test Stubbing using sinon.js", function() {
+        it("Test Accounts.create", function(done) {
+            Accounts.create(function(res) {
+                expect(res).to.equal(100);
+                done();
+            });
+        });
+    });
+    */
+
+    /*
     describe("Test Login", function() {
         var url = "http://localhost:8000/accounts/login";
         it("Successful login and expect 302 - success redirect", function(done) {
@@ -72,4 +90,5 @@ describe("Accounts controller", function() {
             });
         });
     });
+    */
 });
